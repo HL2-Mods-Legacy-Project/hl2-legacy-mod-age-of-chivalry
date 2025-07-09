19e0bfb8a5f4b20ea01037c0ca7d426d038378a1
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2025-07-09 18:54:26 -0400

    Update build_all.ps1

--- scripts/build_all.ps1
+++ scripts/build_all.ps1
@@ -1,12 +1,11 @@
 & "$PSScriptRoot\..\gitmodules\mod-installer\build_all.ps1" `
-  -Version "<version>" `
+  -Version "2.0.0" `
   -PreReleaseVersion "beta.1" `
-  -OutputFilePrefix "<output file prefix>" `
-  -ModName "<mod name>" `
-  -ModFolder "<mod folder>" `
-  -AdditionalModFolders @() `
+  -OutputFilePrefix "AoC_1.1" `
+  -ModName "Age of Chivalry 1.1" `
+  -ModFolder "ageofchivalry" `
   -ModFilesPath "$PSScriptRoot\..\game" `
-  -ModBaseFilesUrl "<url to mod base files zip>" `
-  -ModBaseFilesUrlHash "<mod base files zip sha256>" `
-  -ModReadmePath "<mod readme path>" `
+  -ModBaseFilesUrl "https://github.com/HL2-Mods-Legacy-Project/hl2-legacy-registry/releases/download/age-of-chivalry/ageofchivalry-1.1-Original.zip" `
+  -ModBaseFilesUrlHash "259cf046b626ff4466d3a85fcfc10050fbf520fe62b3e1ad0288892831ad5cc6" `
+  -ModReadmePath "readme.txt" `
   -PublishPath "$PSScriptRoot\..\artifacts\publish"
